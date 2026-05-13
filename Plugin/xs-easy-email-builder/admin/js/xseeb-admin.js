d5fa28dc188bb91a7296b31160719640b339f439
hateebdev <143612694+hateebdev@users.noreply.github.com>
2026-05-13 16:42:46 +0500

    Delete Plugin/xs-easy-email-builder directory
    add new version

--- Plugin/xs-easy-email-builder/admin/js/xseeb-admin.js
+++
@@ -1,7 +0,0 @@
-(function( $ ) {
-	'use strict';
-	document.title = 'Easy Email Editor';
-	localStorage.setItem('xseeb_base_url',xseeb_base_url.xseeb_base_url);
-	localStorage.setItem('xseeb_plugin_url',xseeb_base_url.xseeb_plugin_url);
-	localStorage.setItem('xseeb_wp_nonce',xseeb_base_url.nonce);
-})( jQuery );
